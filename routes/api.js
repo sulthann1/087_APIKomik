f7c7e80d7f914e92740824c322cdb6277c5b8c60
sulthann1 <sulthanaawaliya155@gmail.com>
2026-07-29 10:06:29 +0700

    menambahkan kode di api.js

---
+++ routes/api.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+const komikController = require("../controller/komikController");
+const userController = require("../controller/userController");
+const authMiddleware = require("../middleware/authMiddleware");
+
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+
+router.get("/komik", komikController.getAllKomik);
+router.get("/komik/:id", komikController.getKomikById);
+
+router.post("/komik", authMiddleware, komikController.createKomik);
+router.put("/komik/:id", authMiddleware, komikController.updateKomik);
+router.delete("/komik/:id", authMiddleware, komikController.deleteKomik);
+
+router.get("/genre", authMiddleware, genreController.getAllGenre);
+router.get("/genre/:id", authMiddleware, genreController.getGenreById);
+router.post("/genre", authMiddleware, genreController.createGenre);
+router.put("/genre/:id", authMiddleware, genreController.updateGenre);
+router.delete("/genre/:id", authMiddleware, genreController.deleteGenre);
+
+module.exports = router;
