48189829db57abfc44fe9a4b6b8b0b8c9bfd625d
sulthann1 <sulthanaawaliya155@gmail.com>
2026-07-29 10:29:59 +0700

    fixing something bug

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+const genreController = require("../controller/genreController");
 const komikController = require("../controller/komikController");
 const userController = require("../controller/userController");
 const authMiddleware = require("../middleware/authMiddleware");
